3f2bb0865e22a027f2075775b3ba37af7f51300e
rahul54726 <vermarahul11034@gmail.com>
2026-06-11 16:03:08 +0000

    chore: initial setup with spring boot and gitignore

---
+++ src/main/java/com/npc/kura/KuraApplication.java
@@ -0,0 +1,13 @@
+package com.npc.kura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuraApplication.class, args);
+	}
+
+}
