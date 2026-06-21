92d10ce0369f8f8cce67b9871f0e651316946be4
rahul54726 <vermarahul11034@gmail.com>
2026-06-21 09:31:24 +0000

    Fixed timezone error

--- src/main/java/com/npc/kura/KuraApplication.java
+++ src/main/java/com/npc/kura/KuraApplication.java
@@ -3,10 +3,13 @@ package com.npc.kura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class KuraApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(KuraApplication.class, args);
 	}
 
